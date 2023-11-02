ce9b0a0a75246e5e81e19f88c345a4829bfbcb28
yinzhenyu-su <sharkrain@outlook.com>
2023-11-03 00:15:47 +0800

    feat: 添加拖拽功能

--- src/components/ClassroomBoard.vue
+++ src/components/ClassroomBoard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { reactive, ref, watchEffect } from 'vue';
+import { type UseDraggableReturn, VueDraggable } from 'vue-draggable-plus'
 import RightPanel from './RightPanel.vue'
 
 import type { ClassFormDataType } from './type'
@@ -15,75 +16,104 @@ const handleClassChange = (data: ClassFormDataType) => {
   formData.gapNum = data.gapNum;
 }
 
+type RowItem = {
+  name: string;
+}
+
 type ColItem = {
   /**
-   * 是否有过道
+   * 是否为过道
    */
   segment: boolean;
   /**
    * 第几个过道
    */
   segmentIndex: number;
+  /**
+   * 第几组 
+   */
+  groupIndex: number;
+  rowList: RowItem[];
+  id: number;
 }
 
-const colNum = computed(() => {
+const colList = ref([] as ColItem[])
+
+watchEffect(() => {
   // 分配过道的算法需要根据具体业务来实现，这里提供一个尽可能平均分配的算法
   // 实际最好是可以由用户拖拽过道实现分组
-  const result: ColItem[] = Array.from({length: formData.colNum})
+  const result: ColItem[] = Array.from({ length: formData.colNum })
   result.forEach((d, index) => {
-    result[index] = {segment: false, segmentIndex: 0}
+    const rowList = Array.from({ length: formData.rowNum }) as RowItem[]
+    rowList.fill({ name: '' })
+    rowList.forEach((r, rIndex) => {
+      // 实际业务中学生应该是一个对象，有自己的id，这里直接用名字当作id使用
+      rowList[rIndex] = { name: `张${index}${rIndex}` }
+    })
+    result[index] = { segment: false, segmentIndex: 0, id: index, groupIndex: index, rowList }
   });
 
 
   let segmentSize = Math.floor(result.length / (formData.gapNum + 1))
   let segmentLeft = 0;
-  for(let i = 0; i < result.length; i+=segmentSize) {
+  for (let i = 0; i < result.length; i += segmentSize) {
     const end = i + segmentSize;
     try {
       if (end < result.length && segmentLeft < formData.gapNum) {
-        result[end - 1].segment = true;
-        result[end - 1].segmentIndex = segmentLeft;
+        // 添加过道
+        result.splice(end + segmentLeft, 0, { segment: true, segmentIndex: segmentLeft, id: result.length + 1, groupIndex: end, rowList: [] })
         segmentLeft += 1
       }
-    } catch(e) {
+    } catch (e) {
       throw new Error('分配过道失败')
     }
   }
 
-  return result;
-})
+  colList.value = result;
+});
 
-const rowNum = computed(() => {
-  return new Array(formData.rowNum).fill(0)
-})
+const el = ref<UseDraggableReturn>();
 
+const handleUpdate = () => {
+  // 重新排序
+  colList.value.filter((col) =>
+    col.segment
+  ).forEach((s, sIndex) => {
+    s.segmentIndex = sIndex;
+  })
 
-const showAisle = (colIndex: number) => {
-  return colNum.value[colIndex].segment;
+  colList.value.filter((col) => !col.segment).forEach((g, gIndex) => {
+    g.groupIndex = gIndex;
+  })
 }
 </script>
 <template>
-<div class="wrap">
-  <div class="board-wrap">
-    <div class="board">
-      <!-- 这里不建议使用index作为key -->
-      <div class="stu-col" v-for="(col, colIndex) in colNum" :key="colIndex">
-        <div>
-          <div class="stu-group-leader">第{{ colIndex + 1 }}组</div>
-          <div v-for="(row, rowIndex) in rowNum" :key="rowIndex" class="stu"></div>
+  <div class="wrap">
+    <div class="board-wrap">
+      <VueDraggable v-model="colList" ref="el" class="board" handle=".col-handle" @update="handleUpdate" :animation="150"
+        group="col">
+        <div class="stu-col list" v-for="col in colList" :key="col.id">
+          <div v-show="!col.segment">
+            <div class="stu-group-leader col-handle">第{{ col.groupIndex + 1 }}组</div>
+            <VueDraggable v-model="col.rowList" :animation="150" group="row" class="col-content col-handle">
+              <div v-for="row in col.rowList" :key="row.name" class="stu">{{ row.name }}</div>
+            </VueDraggable>
+          </div>
+          <div class="aisle col-handle" v-show="col.segment">过道 <b>{{ col.segmentIndex + 1 }}</b></div>
         </div>
-        <div class="aisle" v-if="showAisle(colIndex)">过道 <b>{{ col.segmentIndex + 1 }}</b></div>
-      </div>
+      </VueDraggable>
+    </div>
+    <div class="panel-wrap">
+      <RightPanel @changed="handleClassChange" />
     </div>
   </div>
-  <div class="panel-wrap"><RightPanel @changed="handleClassChange"/></div>
-</div>
 </template>
 <style scoped>
 .wrap {
   display: flex;
   margin-top: 100px;
 }
+
 .board-wrap {
   background: white;
   width: 75%;
@@ -93,34 +123,54 @@ const showAisle = (colIndex: number) => {
   padding-right: 24px;
   margin-right: 24px;
 }
+
 .panel-wrap {
   width: 20%;
 }
+
 .board {
   padding: 24px;
   display: flex;
 }
+
 .stu-col {
   display: flex;
   margin-left: 8px;
 }
+
 .stu {
   width: 120px;
   height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: #DEE4E9;
   border-radius: 4px;
   margin-bottom: 8px;
-  margin-right: 8px;
+  cursor: move;
 }
+
+.col-handle {
+  cursor: move;
+}
+
+.col-content {
+  min-height: 400px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  padding: 8px;
+}
+
 .stu-group-leader {
+  min-width: 138px;
   border: 1px solid #EEEEEE;
   line-height: 24px;
-  height: 24px;
   text-align: center;
   margin-bottom: 8px;
-  margin-right: 8px;
   border-radius: 4px;
+  padding: 8px;
 }
+
 .aisle {
   width: 80px;
   background: grey;
